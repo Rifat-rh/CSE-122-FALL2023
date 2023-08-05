158fe6441f5da6c917519acec46564d86d33a1da
MD.Rifat Hossain <129538138+Rifat-rh@users.noreply.github.com>
2023-08-05 23:24:34 +0600

    Delete lab_Problem1.cpp

--- Lab/lab_Problem1.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout<<endl<<endl<<"Hello in my university"<<endl<<"(emtpy line)"<<endl<<"I am a student in it faculty"<<endl<<"(empty line)"<<endl<<"Welcome"<<endl<<endl;
-    return 0;
-}
