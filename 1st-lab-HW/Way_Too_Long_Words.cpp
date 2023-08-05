48d98e563d77d6e59df7ec340cefebf2fd5e7e56
MD.Rifat Hossain <129538138+Rifat-rh@users.noreply.github.com>
2023-08-05 23:56:43 +0600

    Update Way_Too_Long_Words.cpp

--- 1st-lab-HW/Way_Too_Long_Words.cpp
+++ 1st-lab-HW/Way_Too_Long_Words.cpp
@@ -25,8 +25,8 @@ int main(){
         string s; cin>> s;
     int count=0;
     count=s.size();
-    if(count<=10) cout<<s;
-    else cout<<s[0]<<count-2<<s[s.size()-1];
+    if(count<=10) cout<<s<<endl;
+    else cout<<s[0]<<count-2<<s[s.size()-1]<<endl;
     }
     
     return 0;
