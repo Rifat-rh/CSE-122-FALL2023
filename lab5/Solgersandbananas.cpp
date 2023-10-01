729aa52f157f937633e33092479dd74c3da68450
MD.Rifat Hossain <129538138+Rifat-rh@users.noreply.github.com>
2023-10-01 15:17:12 +0600

    Add files via upload

---
+++ lab5/Solgersandbananas.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+   long long int n,k,w,price=0;
+   cin >> k >> n >> w;
+
+    for(int i=1;i<=w;i++){
+        price+=(k*i);
+    }
+   long long int res=price-n;
+   if(res>0) cout<<res;
+   else cout<<0;
+   return 0;
+}
